9f59211808a008903fd48b82c3342501cf155fc7
Slavyana <slaviana95@gmail.com>
2018-09-07 17:06:16 +0300

    Solution for the exam after mine - 01.09.2018

---
+++ DatabasesAdvanced-EFCore/Exam-01.09.2018/01. Model Definition_Skeleton + Datasets/VaporStore/DataProcessor/ExportDto/UserGameDto.cs
@@ -0,0 +1,15 @@
+﻿namespace VaporStore.DataProcessor.ExportDto
+{
+    using System.Xml.Serialization;
+
+    [XmlType("Game")]
+    public class UserGameDto
+    {
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ DatabasesAdvanced-EFCore/Exam-01.09.2018/01. Model Definition_Skeleton + Datasets/VaporStore/DataProcessor/ImportDto/PurchaseDto.cs
@@ -0,0 +1,27 @@
+﻿namespace VaporStore.DataProcessor.ImportDto
+{
+    using System.Xml.Serialization;
+    using System.ComponentModel.DataAnnotations;
+
+    [XmlType("Purchase")]
+    public class PurchaseDto
+    {
+        [Required]
+        [XmlAttribute("title")]
+        public string Title { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[A-Z0-9]{4}-[A-Z0-9]{4}-[A-Z0-9]{4}$")]
+        public string Key { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\d{4}\s\d{4}\s\d{4}\s\d{4}$")]
+        public string Card { get; set; }
+
+        [Required]
+        public string Date { get; set; }
+    }
+}
